5b1aa0f16b724dd8fdb20ea924bb185bcd8e9fdf
Eric Schulte <schulte.eric@gmail.com>
2013-01-12 11:24:21 -0700

    fleshing out public API

--- package.lisp
+++ package.lisp
@@ -1,20 +1,51 @@
 (defpackage #:graph
   (:use :common-lisp :alexandria :metabang-bind)
   (:export
+   :graph
+   :digraph
+   :copy
+   :digraph-of
+   :graph-of
+   :populate
+   :graph-equal
+   ;; Simple Graph Methods
    :edges
+   :edges-w-values
    :nodes
+   :nodes-w-values
    :has-node-p
    :has-edge-p
    :add-node
    :add-edge
    :node-edges
+   :delete-node
    :edge-value
-   :edge-value
-   :make-graph
+   :delete-edge
+   ;; Complex Graph Methods
+   :merge-nodes
+   :merge-edges
+   :edge-neighbors
    :neighbors
-   :dir-neighbors
-   :dir-step
+   :precedents
+   :connected-component
+   :connectedp
+   :connected-components
+   ;; Cycles and strongly connected components
+   :strongly-connected-components
+   :basic-cycles
    :cycles
-   :cycle-connected-components
+   ;; Shortest Path
    :shortest-path
+   ;; Max Flow
+   :residual
+   :add-paths
+   :max-flow
+   ;; Min Cut
+   :min-cut
+   ;; Visualization
+   :to-dot
+   :dot-to-file
+   ;; Serialization
+   :to-plist
+   :from-plist
    ))
