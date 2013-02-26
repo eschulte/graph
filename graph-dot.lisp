524e6430b30675494890ca8da58e8b67320957e3
Thomas Dye <tsd@tsdye.com>
2013-02-26 09:45:15 -1000

    Implemented a subset of node attributes, got rid of color-scheme

--- graph-dot.lisp
+++ graph-dot.lisp
@@ -26,26 +26,47 @@
         (wrp (funcall edge-label edge) " [label=\"~a\"]")
         ";" (list #\Newline)))))
 
-(defun node-to-dot (node &optional node-label color-scheme node-fill-color
-                           node-color node-font-color)
+(defun node-to-dot (node &optional node-label node-color-scheme node-shape
+                           node-fill-color node-color node-font-color
+                           node-comment node-group node-label-loc
+                           node-ordering node-pen-width node-style
+                           node-ext-label)
   (flet ((wrp (val wrapper) (if val (format nil wrapper val) "")))
     (let ((node-label (or node-label (constantly nil)))
+          (node-color-scheme (or node-color-scheme (constantly nil)))
+          (node-shape (or node-shape (constantly nil)))
           (node-fill-color (or node-fill-color (constantly nil)))
           (node-color (or node-color (constantly nil)))
-          (node-font-color (or node-font-color (constantly nil))))
+          (node-font-color (or node-font-color (constantly nil)))
+          (node-comment (or node-comment (constantly nil)))
+          (node-group (or node-group (constantly nil)))
+          (node-label-loc (or node-label-loc (constantly nil)))
+          (node-ordering (or node-ordering (constantly nil)))
+          (node-pen-width (or node-pen-width (constantly nil)))
+          (node-style (or node-style (constantly nil)))
+          (node-ext-label (or node-ext-label (constantly nil))))
       (concatenate 'string
         (format nil "  \"~a\"" node)
-        (format nil " [colorscheme=~a]" color-scheme)
+        (wrp (funcall node-color-scheme node) " [colorscheme=\"~a\"]")
         (wrp (funcall node-label node) " [label=\"~a\"]")
-        (wrp (funcall node-fill-color node)
-             " [style=\"filled\"] [fillcolor=~a]")
+        (wrp (funcall node-shape node) " [shape=~a]")
+        (wrp (funcall node-fill-color node) " [fillcolor=~a]")
         (wrp (funcall node-color node) " [color=~a]")
         (wrp (funcall node-font-color node) " [fontcolor=~a]")
+        (wrp (funcall node-comment node) " [comment=\"~a\"]")
+        (wrp (funcall node-group node) " [group=\"~a\"]")
+        (wrp (funcall node-label-loc node) " [labelloc=\"~a\"]")
+        (wrp (funcall node-ordering node) " [ordering=\"~a\"]")
+        (wrp (funcall node-pen-width node) " [penwidth=~a]")
+        (wrp (funcall node-style node) " [style=\"~a\"]")
+        (wrp (funcall node-ext-label node) " [xlabel=\"~a\"]")
         ";" (list #\Newline)))))
 
 (defgeneric to-dot (graph &key stream node-label edge-label
-                            color-scheme node-fill-color
-                            node-color node-font-color)
+                            node-color-scheme node-shape node-fill-color
+                            node-color node-font-color node-comment
+                            node-group node-label-loc node-pen-width
+                            node-style node-ext-label)
   (:documentation "Print the dot code representing GRAPH. Keyword
 arguments NODE-LABEL and EDGE-LABEL may provide functions which when
 called on a node or edge return a label for that element of the dot
@@ -54,32 +75,44 @@ returns a color that will be used to fill the node."))
 
 (defmethod to-dot ((graph graph)
                    &key (stream t) node-label (edge-label {edge-value graph})
-                     color-scheme node-fill-color node-color node-font-color)
-  (format stream "~a to_dot {graph [colorscheme=~a];~%"
-          (intern (string-downcase (type-of graph)))
-          (or color-scheme "X11"))
+                     node-color-scheme node-shape node-fill-color node-color
+                     node-font-color node-comment node-group node-label-loc
+                     node-ordering node-pen-width node-style node-ext-label)
+  (format stream "~a to_dot {~%"
+          (intern (string-downcase (type-of graph))))
   (mapc [{format stream "~a"}
-        {node-to-dot _ node-label color-scheme node-fill-color
-        node-color node-font-color}]
+        {node-to-dot _ node-label node-color-scheme node-shape node-fill-color
+        node-color node-font-color node-comment node-group node-label-loc
+        node-ordering node-pen-width node-style node-ext-label}]
         (nodes graph))
   (mapc [{format stream "~a"} {edge-to-dot _ (type-of graph) edge-label}]
         (edges graph))
   (format stream "}~%"))
 
 (defgeneric to-dot-file (graph path &key node-label edge-label
-                                      color-scheme node-fill-color
-                                      node-color node-font-color)
+                                      node-color-scheme node-shape
+                                      node-fill-color
+                                      node-color node-font-color node-comment
+                                      node-group node-label-loc node-ordering
+                                      node-pen-width node-style
+                                      node-ext-label)
   (:documentation "Write a dot representation of GRAPH to PATH."))
 
 (defmethod to-dot-file
     ((graph graph) path &key node-label edge-label
-                          color-scheme node-fill-color
-                          node-color node-font-color)
+                          node-color-scheme node-shape node-fill-color
+                          node-color node-font-color node-comment
+                          node-group node-label-loc node-ordering
+                          node-pen-width node-style node-ext-label)
   (with-open-file (out path :direction :output :if-exists :supersede)
     (to-dot graph :stream out :node-label node-label
             :edge-label edge-label :node-fill-color node-fill-color
-            :color-scheme color-scheme :node-color node-color
-            :node-font-color node-font-color)))
+            :node-color-scheme node-color-scheme :node-color node-color
+            :node-font-color node-font-color :node-shape node-shape
+            :node-comment node-comment :node-group node-group
+            :node-label-loc node-label-loc :node-ordering node-ordering
+            :node-pen-width node-pen-width :node-style node-style
+            :node-ext-label node-ext-label)))
 
 (defun from-dot (dot-string)
   "Parse the DOT format string DOT-STRING into a graph.
