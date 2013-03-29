3353f64c0994ef7b8f3e222c3dd7dc5e7c5326af
Thomas Dye <tsd@tsdye.com>
2013-03-28 16:33:42 -1000

    Adjacency, reachability, and distance matrices from graphs

--- graph-matrix.lisp
+++ graph-matrix.lisp
@@ -13,24 +13,177 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (enable-curry-compose-reader-macros))
 
-(defgeneric to-adjacency-foreign-array (graph)
-  (:documentation "Return the adjacency matrix of GRAPH.
-The result is a `gsll' array."))
+(defvar *infinity* 4294967295)
 
-(defmethod to-adjacency-foreign-array ((graph graph))
-  (let ((grid:*default-grid-type* 'grid:foreign-array))
-    (let ((node-index-hash (make-hash-table))
-          (counter -1))
-      (mapc (lambda (node) (setf (gethash node node-index-hash) (incf counter)))
-            (nodes graph))
-      (let ((matrix (grid:make-foreign-array
-                     '(unsigned-byte 8)
-                     :dimensions (list (1+ counter) (1+ counter))
-                     :initial-element 0)))
-        (mapc (lambda-bind ((a b))
-                (setf (grid:aref matrix
-                                 (gethash a node-index-hash)
-                                 (gethash b node-index-hash))
-                      1))
-              (edges graph))
-        matrix))))
+(defgeneric make-identity-matrix (order)
+  (:documentation "Return an identity matrix of order ORDER."))
+
+(defmethod make-identity-matrix (order)
+  (let ((matrix (make-array (list order order)
+                            :element-type '(unsigned-byte 32)
+                            :initial-element 0)))
+    (loop for i from 0 below order do
+         (setf (aref matrix i i) 1))
+    matrix))
+
+(defgeneric make-universal-matrix (rows cols)
+  (:documentation "Return a universal matrix with ROWS rows and COLS
+  columns."))
+
+(defmethod make-universal-matrix (rows cols)
+  (let ((matrix (make-array (list rows cols)
+                            :element-type '(unsigned-byte 32)
+                            :initial-element 1)))
+    matrix))
+
+(defgeneric to-adjacency-matrix-new (graph)
+  (:documentation "Return the adjacency matrix of GRAPH."))
+
+(defmethod to-adjacency-matrix-new ((graph graph))
+  (let ((node-index-hash (make-hash-table))
+        (counter -1))
+    (mapc (lambda (node) (setf (gethash node node-index-hash) (incf counter)))
+          (nodes graph))
+    (let ((matrix
+           (make-array (list (1+ counter) (1+ counter))
+                       :element-type '(unsigned-byte 32)
+                       :initial-element 0)))
+      (mapc (lambda-bind
+             ((a b))
+             (progn
+               (setf (aref matrix
+                           (gethash a node-index-hash)
+                           (gethash b node-index-hash))
+                     1)
+               (setf (aref matrix
+                           (gethash b node-index-hash)
+                           (gethash a node-index-hash))
+                     1)))
+            (edges graph))
+      matrix)))
+
+(defmethod to-adjacency-matrix-new ((graph digraph))
+  (let ((node-index-hash (make-hash-table))
+        (counter -1))
+    (mapc (lambda (node) (setf (gethash node node-index-hash) (incf counter)))
+          (nodes graph))
+    (let (( matrix (make-array (list (1+ counter) (1+ counter))
+                               :element-type '(unsigned-byte 32)
+                               :initial-element 0)))
+      (mapc (lambda-bind ((a b))
+                         (setf (aref matrix
+                                     (gethash a node-index-hash)
+                                     (gethash b node-index-hash))
+                               1))
+            (edges graph))
+      matrix)))
+
+(defgeneric to-reachability-matrix (graph &optional limit)
+  (:documentation "Return the reachability matrix of the graph GRAPH.
+  With the optional argument LIMIT set to an integer in the range 2 to
+  two less than the order of ADJACENCY, produces a limited
+  reachability matrix with paths of length LIMIT or less."))
+
+;;; might check that LIMIT has been set sensibly, throw an error if not?
+
+(defmethod to-reachability-matrix ((graph graph) &optional limit)
+    (let* ((result (make-identity-matrix (length (nodes graph))))
+           (max-power (if limit limit (- (length (nodes graph)) 1)))
+           (adjacency (to-adjacency-matrix-new graph))
+           (adjacency-powers (copy-array adjacency))
+           (len (length (nodes graph))))
+      (loop for i from 0 below (* len len) do
+           (setf (row-major-aref result i)
+                 (+ (row-major-aref result i)
+                    (row-major-aref adjacency i))))
+      (loop for i from 2 to max-power do
+           (setf adjacency-powers (mmult adjacency-powers adjacency))
+           (setf (row-major-aref result i)
+                 (+ (row-major-aref result i)
+                    (row-major-aref adjacency-powers i))))
+      (loop for i from 0 below (* len len) do
+           (when (< 1 (row-major-aref result i))
+             (setf (row-major-aref result i) 1)))
+      result))
+
+(defgeneric reachablep (graph reachability-matrix from to)
+  (:documentation "Returns 1 if TO is reachable from FROM, 0 otherwise."))
+
+(defmethod reachablep ((graph graph) reachability-matrix from to)
+  (let ((node-index-hash (make-hash-table))
+        (counter -1))
+    (mapc (lambda (node) (setf (gethash node node-index-hash) (incf counter)))
+          (nodes graph))
+    (aref reachability-matrix
+          (gethash from node-index-hash)
+          (gethash to node-index-hash))))
+
+(defgeneric strong-components (rd)
+  (:documentation "Given a reachability matrix of a digraph, RD,
+  return a matrix in which the strong component of GRAPH containing
+  node_i is given by the entries of 1 in the ith row (or column)."))
+
+(defmethod strong-components (rd)
+  (let ((rdprime (mtp rd))
+        (len (* (array-dimension rd 0)
+                (array-dimension rd 1)))
+        (result (copy-array rd :element-type '(unsigned-byte 32))))
+    (loop for i from 0 below len do
+         (setf (row-major-aref result i)
+               (* (row-major-aref rd i)
+                  (row-major-aref rdprime i))))
+    result))
+
+(defgeneric to-distance-matrix (graph)
+  (:documentation "Return the distance matrix of the graph GRAPH."))
+
+(defmethod to-distance-matrix ((graph graph))
+  (let* ((adj-mat (to-adjacency-matrix-new graph))
+         (result nil)
+         (adj-n (to-adjacency-matrix-new graph))
+         (len (length (nodes graph))))
+    (setf result (make-array (list len len)
+                             :element-type '(unsigned-byte 32)
+                             :initial-element *infinity*))
+    (loop for i from 0 below len do
+         (setf (aref result i i) 0))
+    (loop for i from 0 below (* len len) do
+         (when (= (row-major-aref adj-mat i) 1)
+           (setf (row-major-aref result i) 1)))
+    (loop for i from 2 below len do
+         (setf adj-n (mmult adj-mat adj-n))
+         (loop for j from 0 below (* len len) do
+              (when
+                  (and (= (row-major-aref result j) *infinity*)
+                       (> (row-major-aref adj-n j) 0))
+                (setf (row-major-aref result j) i))))
+    result))
+
+(defun mmult (a b)
+  (loop
+     with m = (array-dimension a 0)
+     with n = (array-dimension a 1)
+     with l = (array-dimension b 1)
+     with c = (make-array (list m l)
+                          :initial-element 0
+                          :element-type '(unsigned-byte 32))
+     for i below m do
+       (loop for k below l do
+            (setf (aref c i k)
+                  (loop for j below n
+                     sum (* (aref a i j)
+                            (aref b j k)))))
+     finally (return c)))
+
+;; Transpose a mxn matrix A to a nxm matrix B=A'.
+(defun mtp (A)
+  (let* ((m (array-dimension A 0))
+         (n (array-dimension A 1))
+         (B (make-array `(,n ,m)
+                        :initial-element 0
+                        :element-type '(unsigned-byte 32))))
+    (loop for i from 0 below m do
+          (loop for j from 0 below n do
+                (setf (aref B j i)
+                      (aref A i j))))
+    B))
--- package-matrix.lisp
+++ package-matrix.lisp
@@ -3,8 +3,7 @@
         :alexandria
         :metabang-bind
         :curry-compose-reader-macros
-        :graph
-        :gsll)
+        :graph)
   ;; shadow functions defined in both alexandria and gsll
   (:shadow :copy :factorial :standard-deviation :variance :median :mean)
-  (:export ))
+  (:export :to-adjacency-matrix-new :to-reachability-matrix :make-universal-matrix :make-identity-matrix :to-distance-matrix :reachablep :strong-components))
