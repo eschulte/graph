f639a65ee5481473e5315e239c12c1d61c34357c
tsdye <tsd@tsdye.com>
2016-10-15 16:54:52 -1000

    Added test for elementwise product.

--- test/graph-matrix.lisp
+++ test/graph-matrix.lisp
@@ -339,6 +339,15 @@
     (is (not (matrix-entries-different-p (graph-matrix::matrix-sum f0 f1)
                                          (graph-matrix::matrix-sum l0 l1))))))
 
+(deftest lisp-vs-fast-elementwise-product ()
+  (let ((f0 (make-zeros-matrix (make-instance 'fast-matrix) 3 3))
+        (f1 (make-universal-matrix (make-instance 'fast-matrix) 3 3))
+        (l0 (make-zeros-matrix (make-instance 'matrix) 3 3))
+        (l1 (make-universal-matrix (make-instance 'matrix) 3 3)))
+    (is (not (matrix-entries-different-p
+              (graph-matrix::matrix-elementwise-product f0 f1)
+              (graph-matrix::matrix-elementwise-product l0 l1))))))
+
 (deftest lisp-vs-fast-sum-boolean ()
   (let ((f0 (make-zeros-matrix (make-instance 'fast-matrix) 3 3))
         (f1 (make-universal-matrix (make-instance 'fast-matrix) 3 3))
